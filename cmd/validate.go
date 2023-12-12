8e500e048b366b6d784aac683c1ba92147bd35ed
xm1k3 <xm1k3@protonmail.com>
2023-12-13 00:00:39 +0100

    Revert "Validate fix"

--- cmd/validate.go
+++ cmd/validate.go
@@ -6,7 +6,6 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"sync"
@@ -23,12 +22,12 @@ var validateCmd = &cobra.Command{
 When executed, it scans a specified folder for YAML files. Each YAML file is checked for validity. 
 If a template is found to be invalid, it is deleted from the folder.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		path, _ := cmd.Flags().GetString("path")
+		path, _ := rootCmd.Flags().GetString("path")
 
 		var wg sync.WaitGroup
 		filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
 			if err != nil {
-				log.Fatal(err)
+				return err
 			}
 			if info.IsDir() {
 			} else if filepath.Ext(filePath) == ".yaml" {
@@ -68,6 +67,4 @@ func readFile(filePath string) (string, error) {
 
 func init() {
 	rootCmd.AddCommand(validateCmd)
-	validateCmd.Flags().StringP("path", "p", "cent-nuclei-templates", "Root path to save the templates")
-
 }
